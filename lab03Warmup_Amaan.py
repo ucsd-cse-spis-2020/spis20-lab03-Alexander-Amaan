fb7f70958f3803f8c417611373ccde7486b17680
amaansingh160 <69165808+amaansingh160@users.noreply.github.com>
2020-08-10 13:47:26 -0700

    Create lab03Warmup_Amaan.py

---
+++ lab03Warmup_Amaan.py
@@ -0,0 +1,41 @@
+import turtle
+"""
+def drawPicture(theTurtle):
+
+    ''' Draw a simple picture using a turtle '''
+
+    theTurtle.forward(100)
+
+    theTurtle.left(90)
+
+    theTurtle.forward(100)
+
+    theTurtle.left(90)
+
+    theTurtle.forward(100)
+
+    theTurtle.left(90)
+
+    theTurtle.forward(100)
+
+    theTurtle.left(90)    
+
+
+
+myTurtle = turtle.Turtle()  # Create a new Turtle object
+
+drawPicture(myTurtle)   # make the new Turtle draw the shape
+"""
+turtle1 = turtle.Turtle()
+turtle1.color('blue')
+turtle2 = turtle.Turtle()
+
+turtle1.setpos(-50, -50)
+
+turtle2.setpos(200, 100)
+
+turtle1.forward(100)
+
+turtle2.left(90)
+
+turtle2.forward(100)
