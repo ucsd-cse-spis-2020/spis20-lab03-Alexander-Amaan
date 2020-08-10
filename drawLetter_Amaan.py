a753a14246c4af3959327f3b079ee577e2a955f8
amaansingh160 <69165808+amaansingh160@users.noreply.github.com>
2020-08-10 14:05:31 -0700

    Create drawLetter_Amaan.py

---
+++ drawLetter_Amaan.py
@@ -0,0 +1,17 @@
+import turtle
+
+
+Amaan = turtle.Turtle()
+def drawA(theTurtle):
+  theTurtle.left(70)
+  theTurtle.forward(200)
+  theTurtle.right(140)
+  theTurtle.forward(200)
+  theTurtle.left(180)
+  theTurtle.forward(100)
+  theTurtle.left(90)
+  theTurtle.forward(90)
+
+
+drawA(Amaan)
+  
